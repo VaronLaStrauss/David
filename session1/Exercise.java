879aebd8abc58a644faf5926e59ad508b07550c1
Viany Manuel <jeangmanuel@gmail.com>
2023-04-14 20:49:54 +0200

    Added exercise

---
+++ session1/Exercise.java
@@ -0,0 +1,19 @@
+package session1;
+
+public class Exercise {
+  public static void main(String[] args) {
+    // declare and instantiate an integer with an initial value -20
+
+    // print it
+
+    // create another variable where you add 20 multiplied by -3
+
+    // create another variable where you first add 20 then multiply by -3
+
+    // print the comparison between the two values
+
+    // print the reason why they are the same or different
+
+    // print the two values
+  }
+}
