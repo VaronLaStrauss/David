80a0966d5a2386067681caf820cf34c43a417cf4
Viany Manuel <jeangmanuel@gmail.com>
2023-04-15 12:23:31 +0200

    Added strings with exercise

---
+++ session2/Strings.java
@@ -0,0 +1,58 @@
+package session2;
+
+public class Strings {
+  public static void main(String[] args) {
+    // Java is an object-oriented programming language
+    String str1 = "I am Viany";
+    System.out.println(str1);
+    String str2 = new String("I am Viany");
+    System.out.println(str2);
+
+    System.out.println(str1 == "I am Viany");
+    System.out.println(str2 == "I am Viany");
+
+    System.out.println(str1 == str2);
+    System.out.println(str1.equals(str2));
+
+    // Check address
+    System.out.println(System.identityHashCode(str1));
+    System.out.println(System.identityHashCode("I am Viany"));
+    System.out.println(System.identityHashCode(str2));
+
+    char[] arr = str1.toCharArray();
+    str1.toLowerCase(); // "i am viany"
+    str1.toUpperCase(); // "I AM VIANY"
+
+    // I am Viany
+    // 0123456789
+    String a = str1.substring(2, 3); // a
+    a = a.toUpperCase(); // A
+    System.out.println(a);
+
+    String cap = str1.substring(0, 2) + a + str1.substring(3); // I Am Viany
+    System.out.println(cap);
+  }
+
+  static void exercise() {
+    // create a string with your full name in all lowercase letters
+
+    // create a new string using the new String syntax with the same value as the
+    // previous one
+
+    // print the comparison of the two values
+
+    // why are they different?
+
+    // print the upper case of the first variable
+
+    // separate the first variable into two variables, your first name and last name
+
+    // convert the first name to all upper case except the last letter and store to
+    // a variable
+
+    // convert the last name to all lower case except the firt letter and store to a
+    // variable
+
+    // print the concatenation (+) in one println
+  }
+}
