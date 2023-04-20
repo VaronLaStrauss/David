b5d6bd75a7909d35de2423306580bc5713a25fa5
Viany Manuel <jeangmanuel@gmail.com>
2023-04-20 14:13:28 +0200

    Added exercise for session 2

---
+++ session2/Exercise.java
@@ -0,0 +1,15 @@
+package session2;
+
+public class Exercise {
+  public static void main(String[] args) {
+    // goal: use a string containing letters from a to z (a total 26 letters), and
+    // then change each letter to match your full name
+    // example: abcdefg... --> Viany (all other letters are discarded)
+    // note: use only up to 26 characters of your name
+    // note: if your name is greater than 26 characters, don't add more characters
+    // at the end
+
+    // abcdefghijkl...z
+    // viany manuel
+  }
+}
