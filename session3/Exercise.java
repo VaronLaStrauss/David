7dd2e1b826980ad97cb52a15df63a379d9deaf18
Viany Manuel <jeangmanuel@gmail.com>
2023-04-22 12:08:03 +0200

    Added exercise for loops

---
+++ session3/Exercise.java
@@ -0,0 +1,33 @@
+package session3;
+
+public class Exercise {
+  public static void main(String[] args) {
+    exercise1(1, 52, 31);
+    exercise2("racecar");
+    exercise3(5);
+  }
+
+  static void exercise1(int hours, int minutes, int seconds) {
+    // Create a digital timer that displays <hh>:<mm>:<ss>
+    // it doesn't have to follow the actual time
+    // we just need to see it count down
+  }
+
+  static void exercise2(String word) {
+    // Palindrome
+    // compare the letters to find out if the word is a palindrome
+    // palindrome is when a word is flipped, it still produces the same word
+    // e.g., racecar
+  }
+
+  static void exercise3(int treeSize) {
+    // Asterisk tree
+    // the hieght of the tree is determined by treeSize
+    // Sample tree with size 4
+    // *
+    // **
+    // ***
+    // ****
+    // ||
+  }
+}
