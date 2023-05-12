bb2dbb7b645d6863da68a23bc5d8731bec9adace
Viany Manuel <jeangmanuel@gmail.com>
2023-05-12 17:10:12 +0200

    Added exercise for methods

---
+++ session5/Exercise.java
@@ -0,0 +1,23 @@
+package session5;
+
+public class Exercise {
+  public static void main(String[] args) {
+    // call the method
+  }
+
+  // create a static method that creates a pine tree up to a certain depth by
+  // calling the next method
+  // at the end of the tree is a bark, print out "||"
+  // when the depth is 5, it prints out:
+  // *
+  // * *
+  // * * *
+  // * * * *
+  // * * * * *
+  // ||
+
+  // create a static method that prints out "* " (with the space) up to a certain
+  // amount
+  // e.g., when the amount is 3, it prints out "* * * "
+
+}
